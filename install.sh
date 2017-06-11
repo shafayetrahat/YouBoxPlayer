c23fd199d5a35b5ef3d850d00215d451c8658cb8
shafayetrahat <shafayetrahat@gmail.com>
2017-06-12 05:29:53 +0600

    Create install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 sudo apt-get install mpv &&
 sudo pip install --upgrade google-api-python-client &&
-sudo apt-get install python-qt4
+sudo apt-get install python-qt4 && 
+sudo mv ytpl /usr/local/bin/ &&
+sudo mv a.desktop ~/Desktop && mkdir ~/.youtube_player
+sudo mv * ~/.youtube_player/
