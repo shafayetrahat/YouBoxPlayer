ed275ebc7e04f313200d3837be81fa1e30357c5a
shafayetrahat <shafayetrahat@gmail.com>
2021-12-11 01:37:13 +0600

    Update search.py

--- search.py
+++ search.py
@@ -10,7 +10,7 @@ import os
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyD7QxHIPVfeUaNChhU_DEln-cMDOWM9S0A"
+DEVELOPER_KEY = "*******************************************"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
